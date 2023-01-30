3f18f199d010d74ec0ce2e9a267f45fbc831fa95
Radon10043 <772577952@qq.com>
2023-01-30 13:59:21 +0800

    Fix typo

--- scripts/fuzz/binutils/binutils-asan.sh
+++ scripts/fuzz/binutils/binutils-asan.sh
@@ -1,8 +1,8 @@
 ###
 # @Author: Radon
 # @Date: 2023-01-27 14:20:09
-# @LastEditors: Radon
-# @LastEditTime: 2023-01-27 15:40:11
+ # @LastEditors: Radon
+ # @LastEditTime: 2023-01-30 13:59:07
 # @Description: Hi, say something
 ###
 git clone git://sourceware.org/git/binutils-gdb.git SRC
@@ -20,4 +20,4 @@ mkdir obj-asan && cd obj-asan
 CFLAGS="-fsanitize=address -DFORTIFY_SOURCE=2 -fstack-protector-all -fno-omit-frame-pointer -g -Wno-error" LDFLAGS="-ldl -lutil" ../configure --disable-shared --disable-gdb --disable-libdecnumber --disable-readline --disable-sim --disable-ld
 make clean all
 
-# for f in `ls | grep id`; do echo -e "\n${f}" | cut -d ',' -f 1-3 >> 0.txt | cat $f | /home/radon/Documents/fuzzing/TEMP/binutils/ASAN/obj-asan/binutils/cxxfilt 2>&1 | grep '#[0-2] ' | tee -a 0.txt; done
+# for f in `ls | grep id`; do echo -e "\n${f}" | cut -d ',' -f 1-3 >> 0.txt | cat $f | /home/radon/Documents/fuzzing/CIProjs/binutils/ASAN/obj-asan/binutils/cxxfilt 2>&1 | grep '#[0-2] ' | tee -a 0.txt; done
