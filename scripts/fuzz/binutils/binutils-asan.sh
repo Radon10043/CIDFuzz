75159060578c3e01d694e110ddd9afa0ebc908a9
Radon10043 <772577952@qq.com>
2023-02-08 12:02:06 +0800

    Update ASAN script

--- scripts/fuzz/binutils/binutils-asan.sh
+++ scripts/fuzz/binutils/binutils-asan.sh
@@ -1,8 +1,8 @@
 ###
 # @Author: Radon
 # @Date: 2023-01-27 14:20:09
-# @LastEditors: Radon
-# @LastEditTime: 2023-02-03 14:21:00
+ # @LastEditors: Radon
+ # @LastEditTime: 2023-02-06 15:02:25
 # @Description: Hi, say something
 ###
 read -p "Do you want to patch CVE-2016-4487? y/n: " res
@@ -20,7 +20,7 @@ export ASAN_OPTIONS="detect_leaks=0"
 
 # Patch CVE-2016-4487
 if [ "$res" == "y" ]; then
-    sed -i "1239a work->bsize = 0;" libiberty/cplus-dem.c
+    cp ../patches/patch-2016-4487-cplus-dem.c ./libiberty/cplus-dem.c
 fi
 
 mkdir obj-asan && cd obj-asan
@@ -28,3 +28,5 @@ CFLAGS="-fsanitize=address -DFORTIFY_SOURCE=2 -fstack-protector-all -fno-omit-fr
 make clean all
 
 # for f in `ls | grep id`; do echo -e "\n${f}" | cut -d ',' -f 1-3 >> 0.txt | cat $f | /home/radon/Documents/fuzzing/CIProjs/binutils/ASAN/obj-asan/binutils/cxxfilt 2>&1 | egrep 'SUMMARY|#[0-9] ' | tee -a 0.txt; done
+
+# CFLAGS="-DFORTIFY_SOURCE=2 -fstack-protector-all -fno-omit-frame-pointer -g -Wno-error" LDFLAGS="-ldl -lutil" ../configure --disable-shared --disable-gdb --disable-libdecnumber --disable-readline --disable-sim --disable-ld
