06b49ffdfd9dceea4f1b8638956207750d5724a2
Radon10043 <772577952@qq.com>
2023-02-22 15:14:32 +0800

    Update binutils scripts

--- scripts/fuzz/binutils/binutils-asan.sh
+++ scripts/fuzz/binutils/binutils-asan.sh
@@ -1,12 +1,14 @@
 ###
 # @Author: Radon
-# @Date: 2023-01-27 14:20:09
- # @LastEditors: Radon
- # @LastEditTime: 2023-02-06 15:02:25
+# @Date: 2023-02-03 14:45:54
+# @LastEditors: Radon
+# @LastEditTime: 2023-02-22 15:13:41
 # @Description: Hi, say something
 ###
-read -p "Do you want to patch CVE-2016-4487? y/n: " res
 
+#!/bin/bash
+
+read -p "Do you want to patch CVE-2016-4487? y/n: " res
 git clone git://sourceware.org/git/binutils-gdb.git SRC
 rm -rf ASAN
 cp -r SRC ASAN
@@ -16,17 +18,21 @@ git checkout 2c49145
 export CC=clang
 export CXX=clang++
 export LDFLAGS=-lpthread
-export ASAN_OPTIONS="detect_leaks=0"
+export ASAN_OPTIONS="detect_leaks=0 abort_on_error=1 symbolize=0"
 
 # Patch CVE-2016-4487
+res="y"
 if [ "$res" == "y" ]; then
-    cp ../patches/patch-2016-4487-cplus-dem.c ./libiberty/cplus-dem.c
+    cd ..
+    cp -r patches/2016-4487/* ASAN/
+    cd ASAN
+    touch 0.patch.radon.txt
 fi
 
 mkdir obj-asan && cd obj-asan
-CFLAGS="-fsanitize=address -DFORTIFY_SOURCE=2 -fstack-protector-all -fno-omit-frame-pointer -g -Wno-error" LDFLAGS="-ldl -lutil" ../configure --disable-shared --disable-gdb --disable-libdecnumber --disable-readline --disable-sim --disable-ld
+CFLAGS="-fsanitize=address -U_FORTIFY_SOURCE -DFORTIFY_SOURCE=2 -fstack-protector-all -fno-omit-frame-pointer -g -Wno-error" LDFLAGS="-ldl -lutil" ../configure --disable-shared --disable-gdb --disable-libdecnumber --disable-readline --disable-sim --disable-ld
 make clean all
 
-# for f in `ls | grep id`; do echo -e "\n${f}" | cut -d ',' -f 1-3 >> 0.txt | cat $f | /home/radon/Documents/fuzzing/CIProjs/binutils/ASAN/obj-asan/binutils/cxxfilt 2>&1 | egrep 'SUMMARY|#[0-9] ' | tee -a 0.txt; done
+# for f in `ls | grep id`; do echo -e "\n${f}" | cut -d ',' -f 1-3 >> 0.txt; cat $f | /home/radon/Documents/fuzzing/CIProjs/binutils/ASAN/obj-asan/binutils/cxxfilt 2>&1 | egrep 'SUMMARY|#[0-9] ' | tee -a 0.txt; done
 
 # CFLAGS="-DFORTIFY_SOURCE=2 -fstack-protector-all -fno-omit-frame-pointer -g -Wno-error" LDFLAGS="-ldl -lutil" ../configure --disable-shared --disable-gdb --disable-libdecnumber --disable-readline --disable-sim --disable-ld
--- scripts/fuzz/binutils/binutils-check.sh
+++ scripts/fuzz/binutils/binutils-check.sh
@@ -2,13 +2,67 @@
 # @Author: Radon
 # @Date: 2023-01-27 14:20:09
 # @LastEditors: Radon
-# @LastEditTime: 2023-01-30 13:52:49
+# @LastEditTime: 2023-02-22 15:12:57
 # @Description: Hi, say something
 ###
 
-for ((i = 1; i <= 20; i++)); do
-    cd out$i/crashes
-    if [ -f '0.txt' ]; then rm 0.txt; fi
-    for f in $(ls | grep id); do echo -e "\n${f}" | cut -d ',' -f 1-3 >>0.txt | cat $f | /home/radon/Documents/fuzzing/CIProjs/binutils/ASAN/obj-asan/binutils/cxxfilt 2>&1 | egrep '(SUMMARY|#[0-9])' | tee -a 0.txt; done
-    cd ../../
+asan() {
+    for ((i = 1; i <= 20; i++)); do
+        cd out$i/crashes
+        if [ -f '0.asan.txt' ]; then rm 0.asan.txt; fi
+        cnt=0
+        for f in $(ls | grep id); do
+            echo -n 'Analyzing out'$i' ... '$cnt$'\r'
+            echo -e "\n${f}" | cut -d ',' -f 1-3 >>0.asan.txt
+            cat $f | /home/radon/Documents/fuzzing/CIProjs/binutils/ASAN/obj-asan/binutils/cxxfilt 2>&1 | egrep '(SUMMARY|#[0-9]|Hint)' | tee -a 0.asan.txt >/dev/null
+            ((cnt++))
+        done
+        echo
+        cd ../../
+    done
+}
+
+patch() {
+    for ((i = 1; i <= 20; i++)); do
+        cd out$i/crashes
+        if [ -f '0.patch.'$1'.txt' ]; then rm 0.patch.$1.txt; fi
+        cnt=0
+        for f in $(ls | grep id); do
+            echo -e "\n${f}" | cut -d ',' -f 1-3 >>0.patch.$1.txt
+            cat $f | /home/radon/Documents/fuzzing/CIProjs/binutils/PATCH-2016-$1/obj-build/binutils/cxxfilt >/dev/null 2>&1
+            extnum=$?
+
+            echo $extnum >>0.patch.$1.txt
+            if [ $extnum -eq 0 ]; then
+                echo -n 'Analyzing out'$i' ... '$cnt$' ... yeah!\r'
+                break
+            fi
+
+            echo -n 'Analyzing out'$i' ... '$cnt$'\r'
+            ((cnt++))
+        done
+        echo
+        cd ../../
+    done
+}
+
+FUZZER=asan-skip-det/afl
+NUMBERS=(4487 4489 4490 4491 4492 6131)
+
+for NUMBER in ${NUMBERS[@]}; do
+    if [ ! -d 'PATCH-2016-'$NUMBER ]; then
+        echo "Directory PATCH-2016-"$NUMBER" doesn't exist!"
+        exit 1
+    fi
+done
+
+cd res/$FUZZER
+for NUMBER in ${NUMBERS[@]}; do
+    if [ $NUMBER -eq 4488 ]; then
+        echo "Analyzing $NUMBER in ASAN ..."
+        asan
+    else
+        echo "Analyzing $NUMBER ..."
+        patch $NUMBER
+    fi
 done
